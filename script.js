ca422aa7356fedef524bf8bcc2f39ac245600986
Will Rodgers <WillRodgers.e.s@gmail.com>
2020-07-24 21:31:45 -0700

    New Upload

--- script.js
+++ script.js
@@ -24,119 +24,152 @@ var x = setInterval(function () {
 		document.getElementById("demo").innerHTML = "EXPIRED";
 	}
 }, 1000);
-//blue element
-var element = document.createElement("div");
-element.style.cssText = "width 200px; height200px; background:blue";
-element.textContent = "example";
-element.onclick = function () {
-	alert("Howdy");
-};
-document.body.appendChild(element);
+
+
+
 
 // questions
-var currentQuestionIndex = 0;
-
 const myQuestions = [{
 		question: "What dose HTML Stand for?",
-		answers: {
-			a: "Hyper Text Markup Language",
-			b: "Hyper Text Markdown Language",
-			c: "Hold Turn Marketing Language",
-			d: "Hyper Texting Modual Languages",
-		},
-		correctAnswer: "a",
+		answers: [
+			"Hyper Text Markup Language",
+			"Hyper Text Markdown Language",
+			"Hold Turn Marketing Language",
+			"Hyper Texting Modual Languages",
+		],
+		correctAnswer: "Hyper Text Markup Language"
 	},
 
 	{
 		question: "The condition in an if / else statment is enclosed with ?",
-		answers: {
-			a: "Quotes",
-			b: "Curly Brackets",
-			c: "Parantheses",
-			d: "Square Brackets",
-		},
-		correctAnswer: "c",
+		answers: [
+			"Quotes",
+			"Curly Brackets",
+			"Parantheses",
+			"Square Brackets",
+		],
+		correctAnswer: "Parantheses"
 	},
 
 	{
 		question: "Arrays in javascript can be used to store?",
-		answers: {
-			a: "Numbers and Strings",
-			b: "Other Arrays",
-			c: "Booleans",
-			d: "All of the Above",
-		},
-		correctAnswer: "d",
+		answers: [
+			"Numbers and Strings",
+			"Other Arrays",
+			"Booleans",
+			"All of the Above",
+		],
+		correctAnswer: "All of the Above",
 	},
 
 	{
 		question: "String Values must be closed within _____ when being assigned to varibles?",
-		answers: {
-			a: "Commas",
-			b: "Curly Brackets",
-			c: "Quotes",
-			d: "Parantheses",
-		},
-		correctAnswer: "b",
+		answers: [
+			"Commas",
+			"Curly Brackets",
+			"Quotes",
+			"Parantheses",
+		],
+		correctAnswer: "Curly Brackets",
 	},
 
 	{
 		question: "Bootstrsp is which of the following?",
-		answers: {
-			a: "Language",
-			b: "Command",
-			c: "Workspace",
-			d: "Framework",
-		},
-		correctAnswer: "d",
+		answers: [
+			"Language",
+			"Command",
+			"Workspace",
+			"Framework",
+		],
+		correctAnswer: "Framework",
 	},
 
 	{
 		question: "If you want to view your code on a web browser you should right click then select?",
-		answers: {
-			a: "Inspect",
-			b: "Cast",
-			c: "Back",
-			d: "Reload",
-		},
-		correctAnswer: "a",
+		answers: [
+			"Inspect",
+			"Cast",
+			"Back",
+			"Reload",
+		],
+		correctAnswer: "Inspect",
 	},
 
 	{
 		question: "Varribles are?",
-		answers: {
-			a: "Bullion",
-			b: "Servers",
-			c: "Placeholders",
-			d: "Calls",
-		},
-		correctAnswer: "c",
+		answers: [
+			"Bullion",
+			"Servers",
+			"Placeholders",
+			"Calls",
+		],
+		correctAnswer: "Placeholders",
 	},
 ];
 
+var currentQuestionIndex = 0;
 var screenDiv = document.querySelector("#screen");
+var questionDiv = document.querySelector("#question-div");
+var gameDiv = document.querySelector("#game");
+var questionLine = document.getElementById("question");
+var choicesLine = document.getElementById("choices");
 
+
+
+// button start
 function startQuiz() {
 	console.log("Did I make it here?");
+	var thisQuestion = myQuestions[currentQuestionIndex];
+	console.log(thisQuestion)
 	screenDiv.innerText = "";
-	screenDiv.innerText = myQuestions[0].question;
-	console.log("myQuestions[0].question: ", myQuestions[0].question);
+	questionLine.textContent = thisQuestion.question;
 
-	// create a for loop to list your answers:
-	(var i = 0; i < myQuestions[0].answers.length; i++) {
-		// 	you will need to iterate through your answers and list each one of them
-		// 	/You will likely want to create a button for each answer to select the right or wrong answer.
-		// }
 
-		function nextQuestion() {
-			// clears DOM
-			// makes an element for display question selected with currentQuestionIndex
-			// makes elements for each choice
-			// adds event listeners to each choice element that call nextQuestion on click
-			// append question element and choice elements to DOM
-			currentQuestionIndex++;
-		}
-		// instead of calling startQuiz right when the script runs
-		startQuiz();
-		// let's assign it to a button click
-		document.querySelector("#bigRed").addEventListener("click", startQuiz);
+	// create a for loop to list your questions:
+	for (var i = 0; i < thisQuestion.answers.length; i++) {
+
+		var answerChoice = document.createElement("button");
+		answerChoice.setAttribute("value", thisQuestion.answers[i]);
+		answerChoice.setAttribute("class", "choice");
+		answerChoice.textContent = thisQuestion.answers[i];
+		answerChoice.onclick = answerSelector;
+		choicesLine.appendChild(answerChoice);
+	}
+
+}
+
+// subtract time & enter intials, give high score
+function answerSelector() {
+	console.log("answer checker")
+	if (this.value != myQuestions[currentQuestionIndex].correctAnswer) {
+		console.log("incorrect");
+		// subtract time & enter intials, give high score
+
+	} else {
+		console.log("correct")
+
+	}
+	currentQuestionIndex++;
+	nextQuestion();
+}
+
+// create a for loop to list your answers:
+function nextQuestion() {
+	choicesLine.textContent = "";
+	var thisQuestion = myQuestions[currentQuestionIndex];
+	questionLine.textContent = thisQuestion.question;
+
+	for (var i = 0; i < thisQuestion.answers.length; i++) {
+
+		var answerChoice = document.createElement("button");
+		answerChoice.setAttribute("value", thisQuestion.answers[i]);
+		answerChoice.setAttribute("class", "choice");
+		answerChoice.textContent = thisQuestion.answers[i];
+		answerChoice.onclick = answerSelector;
+		choicesLine.appendChild(answerChoice);
+	}
+
+}
+
+// let's assign it to a button click
+document.querySelector("#bigRed").addEventListener("click", startQuiz);
